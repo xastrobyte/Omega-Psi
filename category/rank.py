ea606118e1b75d9c833e89271b1f4e82566aa5cc
Jonah Pierce <fellowhashbrown@gmail.com>
2018-10-28 17:50:27 -0600

    Added multiple prefix detection

--- category/rank.py
+++ category/rank.py
@@ -1,7 +1,6 @@
 from category.category import Category
 
 from util.command.command import Command
-from util.file.omegaPsi import OmegaPsi
 from util.file.server import Server
 from util.level.image import createRankImage
 
@@ -62,10 +61,10 @@ class Rank(Category):
         """
 
         # Make sure message starts with the prefix
-        if message.content.startswith(OmegaPsi.PREFIX) and not message.author.bot:
+        if Server.startsWithPrefix(message.guild, message.content) and not message.author.bot:
 
             # Split up into command and parameters if possible
-            command, parameters = Category.parseText(message.content)
+            command, parameters = Category.parseText(Server.getPrefixes(message.guild), message.content)
             
             # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
