1bb28eea53914526ccd50c62ff23d58ef2049c01
Jonah Pierce <fellowhashbrown@gmail.com>
2018-12-22 17:43:01 -0700

    Added Discord Bots List server count updating

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from util.file.server import Server
 
 from keepAlive import keepAlive
 from util.utils.discordUtils import sendMessage, sendErrorMessage
+from util.utils.stringUtils import PROFANE_WORDS
 
 from discord.ext.commands import Bot
 from random import randint
@@ -67,16 +68,56 @@ async def on_message(message):
         # Check if message was in server
         if message.guild != None:
 
-            # Update experience; Get message
-            message = await Server.updateExperience(message)
+            # Check if profanity_filter is not active in the server
+            if not await Server.isProfanityFilterActive(message.guild):
 
-            # Rank up message existed
-            if message != None:
+                # Update experience; Get message
+                message = await Server.updateExperience(message)
 
-                await sendMessage(
-                    omegaPsi, origMessage,
-                    message = message, plain = True
-                )
+                # Rank up message existed
+                if message != None:
+
+                    await sendMessage(
+                        omegaPsi, origMessage,
+                        message = message, plain = True
+                    )
+            
+            # profanity_filter is active. delete message, tell user not to use profanity
+            else:
+
+                # Check if message has profanity
+                profanity = [word for word in PROFANE_WORDS if word in message.content]
+                if len(profanity) > 0:
+
+                    # Try to delete their message
+                    try:
+                        await message.delete()
+                    
+                    # Couldn't delete message because of Forbidden
+                    # Send message to server owner, if dm_on_fail is True, saying that profanity filter is active
+                    #    but Omega Psi doesn't have permission to delete the message
+                    except discord.Forbidden:
+
+                        # Make sure owner is not None (That has happened before)
+                        if message.guild.owner != None and await Server.dmOwnerOnProfanityFail(message.guild):
+                            await message.guild.owner.send(
+                                embed = discord.Embed(
+                                    title = "Missing Permissions",
+                                    description = "You have the Profanity Filter activated but I can't delete messages that contain profanity.",
+                                    colour = 0xFF0000
+                                ).set_author(
+                                    name = "In Server: {}".format(
+                                        message.guild.name
+                                    )
+                                )
+                            )
+                    
+                    await origMessage.channel.send(
+                        "{} you're not supposed to use profanity...".format(
+                            message.author.mention
+                        ),
+                        delete_after = 5
+                    )
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Member Events
@@ -85,34 +126,67 @@ async def on_message(message):
 @omegaPsi.event
 async def on_member_join(member):
 
-    # Open server file
-    server = await Server.openServer(member.guild)
-
     # Update member
     await Server.updateMember(member.guild, member)
+
+    # Open server file
+    server = await Server.openServer(member.guild)
 
     # Send message in welcome message channel
     if server["welcome_message"]["active"]:
         
         # Make sure the welcome message channel exists
         if server["welcome_message"]["channel"]:
+            message = await Server.getWelcomeMessage(member.guild)
+
             await sendMessage(
                 omegaPsi,
                 member.guild.get_channel(
                     await Server.getWelcomeMessageChannel(member.guild)
                 ),
-                message = await Server.getWelcomeMessage(member.guild).format(member.mention)
+                message = message.format(member.mention)
             )
         
         # Send message to owner if they can be grabbed
+        #  and if they want to receive DM's on Fail
         else:
             owner = member.guild.owner
 
-            if owner != None:
+            if owner != None and await Server.dmOwnerOnWelcomeFail(member.guild):
                 await owner.send(
                     embed = discord.Embed(
                         title = "Welcome Channel Not Set",
                         description = "You have the Welcome Message activated but you don't have a channel set. Set it with `omega setWelcomeMessageChannel`",
+                        colour = 0xFF0000
+                    ).set_author(
+                        name = "In Server: {}".format(
+                            member.guild.name
+                        )
+                    )
+                )
+    
+    # Give person role if autorole is active
+    if server["autorole"]["active"]:
+
+        # Make sure the autorole role exists
+        if server["autorole"]["role"]:
+            role = member.guild.get_role(int(server["autorole"]["role"]))
+
+            await member.add_roles(
+                role,
+                reason = "well because they joined. duh"
+            )
+        
+        # Send message to owner if they can be grabbed
+        #   and if they want to receive DM's on Fail
+        else:
+            owner = member.guild.owner
+
+            if owner!= None and await Server.dmOwnerOnAutoroleFail(member.guild):
+                await owner.send(
+                    embed = discord.Embed(
+                        title = "Autorole Not Set",
+                        description = "You have the Autorole feature activated but you don't have a role set. Set it with `omega setAutorole`",
                         colour = 0xFF0000
                     ).set_author(
                         name = "In Server: {}".format(
