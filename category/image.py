26038750b0295af0690d4e54bf890b711329b133
Jonah Pierce <fellowhashbrown@gmail.com>
2018-12-25 13:15:03 -0700

    Added imgur command
    Anonymous album will be made for you when you run the command for the first time. All uploaded pictures will be sent to that album.

--- category/image.py
+++ category/image.py
@@ -1,6 +1,7 @@
 from util.file.database import loop
 from util.file.server import Server
 from util.file.omegaPsi import OmegaPsi
+from util.file.user import User
 
 from util.utils.discordUtils import sendMessage, getErrorMessage
 from util.utils.miscUtils import loadImageFromUrl
@@ -13,11 +14,20 @@ import discord, os, pygame, requests, timchen
 
 pygame.init()
 
+reactions = ["⏪", "⬅", "➡", "⏩"]
+
 class Image(Category):
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Class Fields
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    IMGUR_ALBUM_API = "https://api.imgur.com/3/album"
+    IMGUR_ALBUM_GET_API = "https://api.imgur.com/3/album/{}"
+    IMGUR_IMAGE_API = "https://api.imgur.com/3/image"
+
+    IMGUR_ALBUM_URL = "https://imgur.com/a/{}"
+    IMGUR_IMAGE_URL = "https://i.imgur.com/{}"
 
     AVATAR_LIST = "https://api.adorable.io/avatars/list"
     AVATAR_API  = "https://api.adorable.io/avatars/face/{}/{}/{}/{}"
@@ -76,6 +86,31 @@ class Image(Category):
                 }
             },
             "command": self.gif
+        })
+
+        self._imgur = Command(commandDict = {
+            "alternatives": ["imgur"],
+            "info": "Allows you to upload an image to an anonymous imgur album.",
+            "parameters": {
+                "image": {
+                    "info": "The URL or image file of the image to upload.",
+                    "optional": False,
+                    "accepted": {
+                        "me": {
+                            "alternatives": ["me", "self"],
+                            "info": "Gets your album of images from imgur."
+                        }
+                    }
+                }
+            },
+            "errors": {
+                Image.NOT_ENOUGH_PARAMETERS: {
+                    "messages": [
+                        "In order to upload an image to imgur, you need the image."
+                    ]
+                }
+            },
+            "command": self.imgur
         })
 
         self._dog = Command(commandDict = {
@@ -173,6 +208,7 @@ class Image(Category):
 
         self.setCommands([
             self._gif,
+            self._imgur,
             self._dog,
             self._cat,
             self._avatar,
@@ -180,6 +216,8 @@ class Image(Category):
             self._timchen,
             self._nasaImage
         ])
+
+        self._scrollEmbeds = {}
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Command Methods
@@ -236,6 +274,234 @@ class Image(Category):
                 message,
                 message = result
             )
+    
+    async def imgur(self, message, parameters):
+        """
+        """
+
+        attachments = message.attachments
+        canScroll = False
+
+        # Check for no attachments and no parameters
+        if len(attachments) == 0 and len(parameters) == 0:
+            embed = getErrorMessage(self._imgur, Image.NOT_ENOUGH_PARAMETERS)
+        
+        # There was at least one of either
+        else:
+
+            # Check if the first parameter is in the accepted for getting the album to the images
+            #  and getting a scroll embed to show the images
+            try:
+                if parameters[0] in self._imgur.getAcceptedParameter("image", "me").getAlternatives():
+                    me = True
+                else:
+                    me = False
+            except:
+                me = False
+            
+            # Get user's imgur album ID
+            album = await User.getImgurAlbum(message.author)
+            albumHash = album["hash"]
+            albumId = album["id"]
+
+            # See if user does not have an album attached to them
+            fields = []
+            fieldValue = ""
+            if not albumId:
+                response = await loop.run_in_executor(None,
+                    partial(
+                        requests.post,
+                        Image.IMGUR_ALBUM_API,
+                        data = {
+                            "title": "{}#{}".format(
+                                message.author.name,
+                                message.author.discriminator
+                            ),
+                            "description": "An anonymous imgur album made for the above Discord User"
+                        },
+                        headers = {
+                            "Authorization": "Client-ID {}".format(
+                                os.environ["IMGUR_API_KEY"]
+                            )
+                        }
+                    )
+                )
+                response = response.json()
+
+                # See if album creation failed
+                if response["status"] != 200:
+                    error = response["data"]["error"] + "\n"
+
+                    # Add the error to the result field
+                    if len(fieldValue) + len(error) > OmegaPsi.MESSAGE_THRESHOLD:
+                        fields.append(fieldValue)
+                        fieldValue = ""
+                    fieldValue += error
+                
+                # Album creation did not fail
+                else:
+                    success = "Anonymous Album Created at [this link]({}).\n".format(
+                        Image.IMGUR_ALBUM_URL.format(
+                            response["data"]["id"]
+                        )
+                    )
+
+                    # Add the success message to the result field
+                    if len(fieldValue) + len(success) > OmegaPsi.MESSAGE_THRESHOLD:
+                        fields.append(fieldValue)
+                        fieldValue = ""
+                    fieldValue += success
+
+                    # Set the user's imgur album
+                    albumHash = response["data"]["deletehash"]
+                    albumId = response["data"]["id"]
+                    await User.setImgurAlbum(message.author, {"hash": albumHash, "id": albumId})
+
+            # Not getting their album and images; Adding one
+            if not me:
+
+                # Get url for each attachment
+                for attachment in range(len(attachments)):
+                    attachments[attachment] = attachments[attachment].url
+                
+                attachments += parameters
+                
+                # Iterate through attachments
+                for attachment in attachments:                    
+
+                    # Upload image
+                    response = await loop.run_in_executor(None,
+                        partial(
+                            requests.post,
+                            Image.IMGUR_IMAGE_API,
+                            data = {
+                                "image": attachment,
+                                "album": albumHash
+                            },
+                            headers = {
+                                "Authorization": "Client-ID {}".format(
+                                    os.environ["IMGUR_API_KEY"]
+                                )
+                            }
+                        )
+                    )
+                    print(response.content)
+                    response = response.json()
+
+                    # See if image upload failed
+                    if response["status"] != 200:
+                        error = response["data"]["error"] + "\n"
+
+                        # Add the error to the result field
+                        if len(fieldValue) + len(error) > OmegaPsi.MESSAGE_THRESHOLD:
+                            fields.append(fieldValue)
+                            fieldValue = ""
+                        fieldValue += error
+                    
+                    # Image upload did not fail
+                    else:
+                        success = "Anonymous Image Uploaded and Added to your album. [Here]({}) is the direct link to the image.\n".format(
+                            Image.IMGUR_IMAGE_URL.format(
+                                response["data"]["id"]
+                            )
+                        )
+
+                        # Add the success message to the result field
+                        if len(fieldValue) + len(success) > OmegaPsi.MESSAGE_THRESHOLD:
+                            fields.append(fieldValue)
+                            fieldValue = ""
+                        fieldValue += success
+
+                # Add the trailing result field
+                if len(fieldValue) > 0:
+                    fields.append(fieldValue)
+                    
+                # Create embed
+                embed = discord.Embed(
+                    title = "Results {}".format(
+                        "({} / {})".format(
+                            1, len(fields)
+                        ) if len(fields) > 1 else ""
+                    ),
+                    description = fields[0],
+                    colour = self.getEmbedColor() if message.guild == None else message.author.top_role.color
+                )
+
+                # Add all the fields to the embed
+                count = 1
+                for field in fields[1:]:
+                    count += 1
+                    embed.add_field(
+                        name = "Results {}".format(
+                            "({} / {})".format(
+                                count, len(fields)
+                            ) if len(fields) > 1 else ""
+                        ),
+                        value = field,
+                        inline = False
+                    )
+
+            # Getting the author's images
+            else:
+
+                # Get the list of images
+                album = await loop.run_in_executor(None,
+                    partial(
+                        requests.get,
+                        Image.IMGUR_ALBUM_GET_API.format(
+                            albumId
+                        ),
+                        headers = {
+                            "Authorization": "Client-ID f473d8889fc2daf"
+                        }
+                    )
+                )
+                album = album.json()
+
+                # Create the first embed
+                embed = discord.Embed(
+                    title = "Image {}".format(
+                        "({} / {})".format(
+                            1, len(album["data"]["images"])
+                        ) if len(album["data"]["images"]) > 1 else ""
+                    ),
+                    description = album["data"]["description"],
+                    colour = self.getEmbedColor() if message.guild == None else message.author.top_role.color,
+                    url = album["data"]["link"]
+                ).set_image(
+                    url = None if len(album["data"]["images"]) == 0 else album["data"]["images"][0]["link"]
+                ).set_author(
+                    name = album["data"]["title"],
+                    icon_url = message.author.avatar_url if album["data"]["cover"] == None else Image.IMGUR_IMAGE_URL.format(album["data"]["cover"])
+                )
+                
+                # Create the scrolling embed
+                self._scrollEmbeds[str(message.author.id)] = {
+                    "message": None,
+                    "images": album["data"]["images"],
+                    "value": 0,
+                    "min": 0, 
+                    "max": len(album["data"]["images"]) - 1
+                }
+
+                canScroll = len(album["data"]["images"]) > 0
+        
+        msg = await sendMessage(
+            self.client,
+            message,
+            embed = embed.set_footer(
+                text = "Requested by {}#{}".format(
+                    message.author.name,
+                    message.author.discriminator
+                ),
+                icon_url = message.author.avatar_url
+            )
+        )
+
+        if canScroll:
+            for reaction in reactions:
+                await msg.add_reaction(reaction)
+            self._scrollEmbeds[str(message.author.id)]["message"] = msg
     
     async def dog(self, message, parameters):
         """Returns a random dog from the internet
@@ -537,6 +803,80 @@ class Image(Category):
                         # Run the command but don't try running others
                         await self.run(message, cmd, cmd.getCommand(), message, parameters)
                     break
+    
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Reactions
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+    async def manage_scrolling(self, reaction, member):
+        """Manages the scrolling of any scroll embeds
+        """
+
+        # Check for message ID in scrollable embeds
+        memberId = str(member.id)
+        if memberId in self._scrollEmbeds:
+            initial = self._scrollEmbeds[memberId]["value"]
+
+            # Rewind reaction was added; Move to first field
+            if str(reaction) == "⏪":
+                self._scrollEmbeds[memberId]["value"] = self._scrollEmbeds[memberId]["min"]
+            
+            # Fast Forward reaction was added; Move to last field
+            elif str(reaction) == "⏩":
+                self._scrollEmbeds[memberId]["value"] = self._scrollEmbeds[memberId]["max"]
+            
+            # Arrow Left reaction was added; Move field left
+            elif str(reaction) == "⬅":
+                self._scrollEmbeds[memberId]["value"] -= 1
+                if self._scrollEmbeds[memberId]["value"] < self._scrollEmbeds[memberId]["min"]:
+                    self._scrollEmbeds[memberId]["value"] = self._scrollEmbeds[memberId]["min"]
+            
+            # Arrow Right reaction was added; Move field right
+            elif str(reaction) == "➡":
+                self._scrollEmbeds[memberId]["value"] += 1
+                if self._scrollEmbeds[memberId]["value"] > self._scrollEmbeds[memberId]["max"]:
+                    self._scrollEmbeds[memberId]["value"] = self._scrollEmbeds[memberId]["max"]
+            
+            # Update the scroll embed
+            if self._scrollEmbeds[memberId]["value"] != initial and reaction.message.id == self._scrollEmbeds[memberId]["message"].id:
+                value = self._scrollEmbeds[memberId]["value"]
+
+                # Get the image Id at the current value
+                image = self._scrollEmbeds[memberId]["images"][value]
+
+                # Update the embed
+                currentEmbed = self._scrollEmbeds[str(member.id)]["message"].embeds[0]
+                currentEmbed.title = "Image {}".format(
+                        "({} / {})".format(
+                            value + 1, self._scrollEmbeds[str(member.id)]["max"] + 1
+                        ) if self._scrollEmbeds[str(member.id)]["max"] > 0 else ""
+                    )
+                currentEmbed.set_image(
+                    url = image["link"]
+                )
+                await self._scrollEmbeds[str(member.id)]["message"].edit(
+                    embed = currentEmbed
+                )
+
+    async def on_reaction_add(self, reaction, member):
+        """Determines which reaction was added to a message. Only reactions right now are
+
+        :arrow_left: which tells the embed to scroll back a field.
+        :arrow_right: which tells the embed to scroll forward a field.
+        :rewind: which tells the embed to go back to the beginning.
+        :fast_forward: which tells the embed to go to the end.
+        """
+        await self.manage_scrolling(reaction, member)
+    
+    async def on_reaction_remove(self, reaction, member):
+        """Determines which reaction was removed from a message. Only reactions right now are
+
+        :arrow_left: which tells the embed to scroll back a field.
+        :arrow_right: which tells the embed to scroll forward a field.
+        :rewind: which tells the embed to go back to the beginning.
+        :fast_forward: which tells the embed to go to the end.
+        """
+        await self.manage_scrolling(reaction, member)
 
 def setup(client):
     client.add_cog(Image(client))
