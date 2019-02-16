21228fa3a7f8c1430694c856fb06e00d83299769
Jonah Pierce <fellowhashbrown@gmail.com>
2019-02-16 02:05:38 -0700

    Created custom command checks

--- category/predicates.py
+++ category/predicates.py
@@ -2,26 +2,28 @@ from discord.ext import commands
 
 from database import database
 
-def is_developer():
-    async def predicate(ctx):
-        return await database.is_developer(ctx.author)
-    return commands.check(predicate)
+# Overwrite basic command decorators
+async def is_nsfw_or_private(ctx):
+    return not ctx.guild or ctx.channel.is_nsfw()
 
-async def is_developer_async(ctx):
+async def guild_only(ctx):
+    return ctx.guild
+
+async def is_nsfw_and_guild(ctx):
+    return ctx.guild and ctx.channel.is_nsfw()
+
+async def is_developer(ctx):
     return await database.is_developer(ctx.author)
+
+async def can_manage_guild(ctx):
+    return ctx.author.permissions_in(ctx.channel).manage_guild
+
+# Custom checks
 
 def is_in_guild(guild_id):
     async def predicate(ctx):
         return ctx.guild and ctx.guild.id == guild_id
     return commands.check(predicate)
-
-def is_nsfw_or_private():
-    async def predicate(ctx):
-        return not ctx.guild or ctx.channel.is_nsfw()
-    return commands.check(predicate)
-
-async def is_nsfw_or_private_async(ctx):
-    return not ctx.guild or ctx.channel.is_nsfw()
 
 async def get_prefix(bot, message):
 
