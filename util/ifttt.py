eca273eed5f00b672d4023f6318976889ac8ac32
Jonah Pierce <fellowhashbrown@gmail.com>
2019-07-16 14:27:07 -0400

    Changed how the ifttt_push function works

--- util/ifttt.py
+++ util/ifttt.py
@@ -3,12 +3,13 @@ from functools import partial
 
 from database import loop
 
-def ifttt_push_sync(title, subject, text):
+def ifttt_push_sync(event_name, title, subject, text, *, key = None):
 
-    # Make a post request to IFTTT using the "on_push" event
+    # Make a post request to IFTTT using the event specified
     requests.post(
-        "https://maker.ifttt.com/trigger/on_push/with/key/{}".format(
-            os.environ["IFTTT_WEBHOOK_KEY"]
+        "https://maker.ifttt.com/trigger/{}/with/key/{}".format(
+            event_name,
+            os.environ["IFTTT_WEBHOOK_KEY"] if key == None else key
         ),
         json = {
             "value1": title,
@@ -17,14 +18,15 @@ def ifttt_push_sync(title, subject, text):
         }
     )
 
-async def ifttt_push(title, subject, text):
+async def ifttt_push(event_name, title, subject, text, *, key = None):
 
-    # Make a post request to IFTTT using the "on_push" event
+    # Make a post request to IFTTT using the event specified
     await loop.run_in_executor(None,
         partial(
             requests.post,
-            "https://maker.ifttt.com/trigger/on_push/with/key/{}".format(
-                os.environ["IFTTT_WEBHOOK_KEY"]
+            "https://maker.ifttt.com/trigger/{}/with/key/{}".format(
+                event_name,
+                os.environ["IFTTT_WEBHOOK_KEY"] if key == None else key
             ),
             json = {
                 "value1": title,
