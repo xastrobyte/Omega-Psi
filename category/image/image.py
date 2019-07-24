6adc6a91ae36e8ca11e94d70980a472f28c88575
Jonah Pierce <fellowhashbrown@gmail.com>
2019-07-24 12:56:34 -0400

    Moved dog and cat commands to animal category

--- category/image/image.py
+++ category/image/image.py
@@ -18,9 +18,6 @@ from util.discord import get_reddit_post
 AVATAR_LIST = "https://api.adorable.io/avatars/list"
 AVATAR_API = "https://api.adorable.io/avatars/face/{}/{}/{}/{}"
 
-DOG_API = "https://dog.ceo/api/breeds/image/random"
-CAT_API = "https://api.thecatapi.com/v1/images/search"
-
 IMGUR_ALBUM_API = "https://api.imgur.com/3/album"
 IMGUR_ALBUM_GET_API = "https://api.imgur.com/3/album/{}"
 IMGUR_IMAGE_API = "https://api.imgur.com/3/image"
@@ -333,61 +330,6 @@ class Image(commands.Cog, name = "image"):
                         icon_url = ctx.author.avatar_url if album["data"]["cover"] == None else IMGUR_IMAGE_URL.format(album["data"]["cover"])
                     )
                 )
-
-    @commands.command(
-        name = "dog",
-        aliases = ["doggy"],
-        description = "Sends a random picture of a random dog from the internet.",
-        cog_name = "image"
-    )
-    async def dog(self, ctx):
-        
-        # Call the dog api
-        result = await loop.run_in_executor(None,
-            requests.get,
-            DOG_API
-        )
-        result = result.json()
-
-        await ctx.send(
-            embed = discord.Embed(
-                title = "Dog from the internet",
-                description = " ",
-                colour = await get_embed_color(ctx.author)
-            ).set_image(
-                url = result["message"]
-            )
-        )
-    
-    @commands.command(
-        name = "cat",
-        aliases = ["kitty"],
-        description = "Sends a random picture of a random cat from the internet.",
-        cog_name = "image"
-    )
-    async def cat(self, ctx):
-        
-        # Call that cat api
-        result = await loop.run_in_executor(None,
-            partial(
-                requests.get,
-                CAT_API,
-                headers = {
-                    "x-api-key": os.environ["CAT_API_KEY"]
-                }
-            )
-        )
-        result = result.json()
-
-        await ctx.send(
-            embed = discord.Embed(
-                title = "Cat from the internet",
-                description = " ",
-                colour = await get_embed_color(ctx.author)
-            ).set_image(
-                url = result[0]["url"]
-            )
-        )
     
     @commands.command(
         name = "avatar",
