1c9ccf262eb0e939ac144be53a0c8766c3f4271c
Jonah Pierce <fellowhashbrown@gmail.com>
2019-07-28 18:41:50 -0400

    Moved set_defaults function

--- util/misc.py
+++ util/misc.py
@@ -30,3 +30,21 @@ def get_theme():
         )
 
     return shades
+
+def set_default(default_dict, result_dict):
+    """Sets default values for a dictionary recursively.
+    """
+
+    # Iterate through default values
+    for tag in default_dict:
+
+        # If the tag does not exist in the result dictionary, add it
+        if tag not in result_dict:
+            result_dict[tag] = default_dict[tag]
+        
+        # Tag exists in guild dict, see if tag is a dictionary
+        else:
+            if type(result_dict[tag]) == dict:
+                result_dict[tag] = set_default(default_dict[tag], result_dict[tag])
+    
+    return result_dict
